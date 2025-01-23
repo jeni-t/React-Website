3ee29314534bf88e9f1e3099ff920ad39e75b01e
jeni-t <jenivijay19@gmail.com>
2025-01-23 19:37:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import MainPage from "./MainPage.jsx"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <MainPage/>
+    </BrowserRouter>
+
+
+  )
+}
+
+export default App
